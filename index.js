817bec32485a231766c06f5e2b729a37077caa02
VictorSoltan <victor.soltan.evg@gmail.com>
2022-09-22 21:01:17 +0300

    adding zip files upload oportunity

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express'),
-    https = require('https')
     cors = require('cors');    
     app = express(),
+    mainFolder = './nft_folders'
     testFolder = './nft_parts/',
     fs = require('fs'),
+    os = require('os'),
+    multer  = require('multer'),
+    upload = multer({ dest: "uploads/" }),
+    unzipper = require('unzipper'),
     port = process.env.PORT || 8000;
 
 app.use(cors())
@@ -13,8 +17,16 @@ app.get('/', function (req, res) {
     res.send('Hello World');
 })
 
-app.get('/get_folders', function (req, res) {
-    fs.readdir(testFolder, (err, files) => {
+app.get('/get_main_folders', function (req, res) {
+    fs.readdir(mainFolder, (err, files) => {
+        res.send(files)
+        console.log(files)
+    });
+})
+
+app.post('/get_folders', function (req, res) {
+    console.log(mainFolder+'/'+req.body.folder)
+    fs.readdir(mainFolder+'/'+req.body.folder, (err, files) => {
         res.send(files)
         console.log(files)
         // files.forEach(file => {
@@ -37,23 +49,43 @@ app.post('/get_trait', function(req, res) {
     res.sendFile(`./nft_parts/${req.body.trait}/${req.body.file}`, { root: __dirname });
 })
 
-app.get('/nft', function (req, res) {
-    res.sendFile('./nft_parts/head/03-donkey.svg', { root: __dirname });
-})
+app.post('/upload', upload.single('file'), async function(req, res) {
+    try{
+        console.log('/upload')
+        // const title = req.body.title;
+        const file = req.file;
+      
+        console.log(file.originalname);
+        console.log(file);
+        // file.pipe(unzipper.Extract({ path: 'files' }));
+    
+        await fs.createReadStream(file.path).pipe(unzipper.Extract({ path: 'nft_folders' }));
+        await fs.unlinkSync(file.path)
 
-app.get('/nft1', function (req, res) {
-    res.sendFile('./nft_parts/body/01-deer.svg', { root: __dirname });
-})
+        await new Promise(r => setTimeout(r, 1000));
 
-app.get('/nft2', function (req, res) {
-    res.sendFile('./nft_parts/feet/02-wolf.svg', { root: __dirname });
-})
+        console.log('show folders')
+        fs.readdir(mainFolder, (err, files) => {
+            res.send(files)
+            console.log(files)
+        });
+    }catch(e){
+        console.log(e)
+    }
 
-fs.readdir(testFolder, (err, files) => {
-    // files.forEach(file => {
-        // console.log(file);
-    // });
 });
+
+app.post('/deleteFolder', async function(req, res) {
+    console.log(req.body.folderName)
+    await fs.rmSync(mainFolder+'/'+req.body.folderName, { recursive: true, force: true });
+
+    await new Promise(r => setTimeout(r, 1000));
+
+    fs.readdir(mainFolder, (err, files) => {
+        res.send(files)
+        console.log(files)
+    });
+})
 
  let server = app.listen(port, function () {
     let host = server.address().address
